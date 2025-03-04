40ea8036cfc1bce450d345e9ef30ecbf7f7998fd
arindam9864 <arindam.9864@gmail.com>
2025-03-04 23:45:40 +0530

    maggie

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My Maggie Recipe");
+        Maggie(); 
+    }
+
+    public static void Maggie() {
+        System.out.println("1. Boil water in a pan");
+        System.out.println("2. Now add your special Maggie recipe after this");
+    }
+}
